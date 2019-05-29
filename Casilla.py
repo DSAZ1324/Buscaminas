a2e3343a37c6001a7c128a861aef28612554fe8f
Santiago Arteaga <darteag94278@universidadean.edu.co>
2019-05-29 00:43:51 -0500

    Creacion  y desarrollo de las clases Casilla, Numero_casilla,Mina_casilla, Tablero Creacion arhivo Game que estara propuesto como el start del juego(pendiente a desarrollar)

--- Casilla.py
+++ Casilla.py
@@ -1,2 +1,40 @@
+import random
+
+
 class Casilla:
-    pass
+
+    x = 0
+    y = 0
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.estado = False
+        self.num = random.randint(0, 4)
+        self.valor = ''
+
+    def posicion(self):
+        return self.x, self.y
+
+    def estado(self):
+        return self.estado
+
+    def alerta_bomba(self, opcion):
+        if self.valor == '*':
+            self.estado = False
+        else:
+            self.estado = opcion
+
+    def numero(self):
+        return self.num
+
+    def posible_bomba(self, opcion):
+        if opcion == '*':
+            self.valor = '*'
+        elif opcion == '?':
+            self.valor = '?'
+        else:
+            self.valor = ''
+
+    def obtener_bandera(self):
+        return self.valor
--- Tablero.py
+++ Tablero.py
@@ -1,2 +1,22 @@
-class Tablero():
-    pass
+from Casilla import Casilla
+
+
+class Tablero:
+
+    ancho = 0
+    largo = 0
+
+    def __init__(self, ancho, largo):
+        self.ancho = int(ancho)
+        self.largo = int(largo)
+
+    def crear_tablero(self):
+        tab = []
+        casilla = []
+        for i in range(self.ancho):
+            for j in range(self.largo):
+                if len(casilla) < self.largo:
+                    casilla.append(Casilla(i, j))
+            tab.append(casilla)
+            casilla = []
+        return tab
