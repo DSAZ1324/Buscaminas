4a130ec3a336bb86396f58e2e792995c67837f9a
Santiago Arteaga <darteag94278@universidadean.edu.co>
2019-05-29 19:54:31 -0500

    Creacion pruebas adicionales Tablero y Casilla

--- test_tablero.py
+++ test_tablero.py
@@ -4,9 +4,13 @@ from Tablero import Tablero
 
 class TestTablero(TestCase):
     def test_crear_tablero(self):
-        ancho = 8
-        largo = 8
-        tablero = Tablero(ancho, largo)
+        tablero = Tablero(8, 8)
         tab = tablero.crear_tablero()
         tablero_es = tab
         self.assertEqual(tab, tablero_es)
+
+    def test_ganar(self):
+        tablero = Tablero(8, 8)
+        espero = False
+        resultado = tablero.ganar()
+        self.assertEqual(espero, resultado)
