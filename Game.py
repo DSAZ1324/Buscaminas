1d6a23929092f2dde365d1e64328afc361c75efa
Santiago Arteaga <darteag94278@universidadean.edu.co>
2019-05-29 20:08:27 -0500

    Creacion pruebas adicionales Tablero y Casilla

--- Game.py
+++ Game.py
@@ -4,4 +4,22 @@ from Numero_casilla import Numero
 from Mina_casilla import Mina
 from Tablero import Tablero
 
+
 tab = []
+
+
+def ganar(tab):
+    casillas_libres = 0
+    casillas_reveladas = 0
+    for x in range(len(tab)):
+        for y in range(len(tab[0])):
+            if Casilla.numero(tab[x][y]) == 0:
+                casillas_libres += 1
+    for x in range(len(tab)):
+        for y in range(len(tab[0])):
+            if Casilla.numero(tab[x][y]) == 0 and Casilla.estad(tab[x][y]) == True:
+                casillas_reveladas += 1
+    if casillas_libres == casillas_reveladas:
+        return True
+    else:
+        return False
--- Tablero.py
+++ Tablero.py
@@ -19,20 +19,3 @@ class Tablero:
             tab.append(casilla)
             casilla = []
         return tab
-
-    def ganar(self):
-        casillas_libres = 0
-        casillas_reveladas = 0
-        for x in range(len(Tablero.crear_tablero(self))):
-            for y in range(len(Tablero.crear_tablero(self)[0])):
-                if Casilla.numero(Tablero.crear_tablero(self)[x][y]) == 0:
-                    casillas_libres += 1
-        for x in range(len(Tablero.crear_tablero(self))):
-            for y in range(len(Tablero.crear_tablero(self)[0])):
-                if Casilla.numero(Tablero.crear_tablero(self)[x][y]) == 0 and Casilla.estad(
-                        Tablero.crear_tablero(self)[x][y]) == True:
-                    casillas_reveladas += 1
-        if casillas_libres == casillas_reveladas:
-            return True
-        else:
-            return False
