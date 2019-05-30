c96f537c3dbd7bac4d0ef5755cff340a4b478aba
Santiago Arteaga <darteag94278@universidadean.edu.co>
2019-05-29 20:09:15 -0500

    Creacion pruebas adicionales Tablero y Casilla

--- Game.py
+++ Game.py
@@ -5,3 +5,19 @@ from Mina_casilla import Mina
 from Tablero import Tablero
 
 tab = []
+
+def ganar(tab):
+    casillas_libres = 0
+    casillas_reveladas = 0
+    for x in range(len(tab)):
+        for y in range(len(tab[0])):
+            if Casilla.numero(tab[x][y]) == 0:
+                casillas_libres += 1
+    for x in range(len(tab)):
+        for y in range(len(tab[0])):
+            if Casilla.numero(tab[x][y]) == 0 and Casilla.estad(tab[x][y]) == True:
+                casillas_reveladas += 1
+    if casillas_libres == casillas_reveladas:
+        return True
+    else:
+        return False
