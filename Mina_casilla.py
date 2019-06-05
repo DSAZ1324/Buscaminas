188ea1b92874b112fac7dcf0f02210bee2403427
Dvanegas067 <47580724+Dvanegas067@users.noreply.github.com>
2019-06-05 12:45:08 -0500

    correciones de documentacion en en clase mina

--- Mina_casilla.py
+++ Mina_casilla.py
@@ -6,6 +6,7 @@ class Mina(Casilla):
     def __init__(self, x, y):
         """
         Nos permite ubicar las minas en el tablero en las coordenas de x como del eje y
+        para cada una de las casillas.
 
         :param x: int con las minas ubicadas para el eje en x
         :param y: int con las minas unicadas para el eje en y
