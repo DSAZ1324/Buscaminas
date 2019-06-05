c08a9808d53eb0371f0cc1b9730d088485ab7111
Dvanegas067 <47580724+Dvanegas067@users.noreply.github.com>
2019-06-05 12:43:54 -0500

    correciones de documentacion en clase game

--- Mina_casilla.py
+++ Mina_casilla.py
@@ -5,10 +5,10 @@ class Mina(Casilla):
 
     def __init__(self, x, y):
         """
+        Nos permite ubicar las minas en el tablero en las coordenas de x como de y
 
-
-        :param x:
-        :param y:
+        :param x: int con las minas ubicadas para el eje en x
+        :param y: int con las minas unicadas para el eje en y
         """
         super().__init__(x, y)
         self.num = 1
